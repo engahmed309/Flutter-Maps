937ef4f85d11d153a0edc00761d0af72e30f3878
engahmed309 <enga.abdelwahed@gmail.com>
2023-03-28 22:58:52 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+import 'smaples/map_sample.dart';
+
+void main() {
+  runApp(const FlutterMaps());
+}
+
+class FlutterMaps extends StatelessWidget {
+  const FlutterMaps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.lightBlue[100],
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CurrentLocationScreen(),
+    );
+  }
+}
